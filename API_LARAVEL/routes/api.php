d773ae2fb675122efa1336d336e507760d54ba71
fazle6 <fazlenation@gmail.com>
2024-08-28 16:05:09 +0700

    awal

---
+++ API_LARAVEL/routes/api.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Http\Controllers\ArticleController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+Route::apiResource('articles', ArticleController::class);
